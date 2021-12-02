f1d20156f0c03840fcd809f3a223f5345d1d3a0e
fauziahenni <87329906+fauziahenni@users.noreply.github.com>
2021-12-02 06:44:21 -0800

    Add files via upload

---
+++ P4_percobaan4_1204013/P4_percobaan4_1204013/Form1.Designer.cs
@@ -0,0 +1,265 @@
+﻿namespace P4_percobaan4_1204013
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Nama = new System.Windows.Forms.Label();
+            this.Nik = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Membandingkan = new System.Windows.Forms.Label();
+            this.Email = new System.Windows.Forms.Label();
+            this.Kota = new System.Windows.Forms.Label();
+            this.Visi = new System.Windows.Forms.Label();
+            this.tbChar = new System.Windows.Forms.TextBox();
+            this.tbNum = new System.Windows.Forms.TextBox();
+            this.tbLeng = new System.Windows.Forms.TextBox();
+            this.tbCom1 = new System.Windows.Forms.TextBox();
+            this.tbCom2 = new System.Windows.Forms.TextBox();
+            this.tbregex = new System.Windows.Forms.TextBox();
+            this.tbUpper = new System.Windows.Forms.TextBox();
+            this.tbLower = new System.Windows.Forms.TextBox();
+            this.katasandi = new System.Windows.Forms.Label();
+            this.epbenar = new System.Windows.Forms.ErrorProvider(this.components);
+            this.epwarn = new System.Windows.Forms.ErrorProvider(this.components);
+            this.Tampilkan = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.epbenar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.epwarn)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Nama
+            // 
+            this.Nama.AutoSize = true;
+            this.Nama.Location = new System.Drawing.Point(12, 24);
+            this.Nama.Name = "Nama";
+            this.Nama.Size = new System.Drawing.Size(57, 17);
+            this.Nama.TabIndex = 0;
+            this.Nama.Text = "Nama  :";
+            // 
+            // Nik
+            // 
+            this.Nik.AutoSize = true;
+            this.Nik.Location = new System.Drawing.Point(12, 57);
+            this.Nik.Name = "Nik";
+            this.Nik.Size = new System.Drawing.Size(52, 17);
+            this.Nik.TabIndex = 1;
+            this.Nik.Text = "Nik     :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 89);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 17);
+            this.label3.TabIndex = 2;
+            // 
+            // Membandingkan
+            // 
+            this.Membandingkan.AutoSize = true;
+            this.Membandingkan.Location = new System.Drawing.Point(12, 117);
+            this.Membandingkan.Name = "Membandingkan";
+            this.Membandingkan.Size = new System.Drawing.Size(120, 17);
+            this.Membandingkan.TabIndex = 3;
+            this.Membandingkan.Text = "Membandingkan :";
+            // 
+            // Email
+            // 
+            this.Email.AutoSize = true;
+            this.Email.Location = new System.Drawing.Point(12, 173);
+            this.Email.Name = "Email";
+            this.Email.Size = new System.Drawing.Size(50, 17);
+            this.Email.TabIndex = 5;
+            this.Email.Text = "Email :";
+            // 
+            // Kota
+            // 
+            this.Kota.AutoSize = true;
+            this.Kota.Location = new System.Drawing.Point(12, 205);
+            this.Kota.Name = "Kota";
+            this.Kota.Size = new System.Drawing.Size(53, 17);
+            this.Kota.TabIndex = 6;
+            this.Kota.Text = "Kota   :";
+            // 
+            // Visi
+            // 
+            this.Visi.AutoSize = true;
+            this.Visi.Location = new System.Drawing.Point(12, 233);
+            this.Visi.Name = "Visi";
+            this.Visi.Size = new System.Drawing.Size(36, 17);
+            this.Visi.TabIndex = 7;
+            this.Visi.Text = "visi :";
+            // 
+            // tbChar
+            // 
+            this.tbChar.Location = new System.Drawing.Point(147, 19);
+            this.tbChar.Name = "tbChar";
+            this.tbChar.Size = new System.Drawing.Size(100, 22);
+            this.tbChar.TabIndex = 8;
+            this.tbChar.Leave += new System.EventHandler(this.tbChar_Leave);
+            // 
+            // tbNum
+            // 
+            this.tbNum.Location = new System.Drawing.Point(147, 52);
+            this.tbNum.Name = "tbNum";
+            this.tbNum.Size = new System.Drawing.Size(100, 22);
+            this.tbNum.TabIndex = 9;
+            this.tbNum.Leave += new System.EventHandler(this.tbNum_Leave);
+            // 
+            // tbLeng
+            // 
+            this.tbLeng.Location = new System.Drawing.Point(147, 84);
+            this.tbLeng.Name = "tbLeng";
+            this.tbLeng.Size = new System.Drawing.Size(100, 22);
+            this.tbLeng.TabIndex = 10;
+            this.tbLeng.Leave += new System.EventHandler(this.tbLenght_Leave);
+            // 
+            // tbCom1
+            // 
+            this.tbCom1.Location = new System.Drawing.Point(147, 112);
+            this.tbCom1.Name = "tbCom1";
+            this.tbCom1.Size = new System.Drawing.Size(100, 22);
+            this.tbCom1.TabIndex = 11;
+            this.tbCom1.Leave += new System.EventHandler(this.tbCom1_Leave);
+            // 
+            // tbCom2
+            // 
+            this.tbCom2.Location = new System.Drawing.Point(147, 142);
+            this.tbCom2.Name = "tbCom2";
+            this.tbCom2.Size = new System.Drawing.Size(100, 22);
+            this.tbCom2.TabIndex = 12;
+            this.tbCom2.Leave += new System.EventHandler(this.tbCom2_Leave);
+            // 
+            // tbregex
+            // 
+            this.tbregex.Location = new System.Drawing.Point(147, 168);
+            this.tbregex.Name = "tbregex";
+            this.tbregex.Size = new System.Drawing.Size(100, 22);
+            this.tbregex.TabIndex = 13;
+            this.tbregex.Leave += new System.EventHandler(this.tbregex_Leave);
+            // 
+            // tbUpper
+            // 
+            this.tbUpper.Location = new System.Drawing.Point(147, 200);
+            this.tbUpper.Name = "tbUpper";
+            this.tbUpper.Size = new System.Drawing.Size(100, 22);
+            this.tbUpper.TabIndex = 14;
+            this.tbUpper.Leave += new System.EventHandler(this.tbUpper_Leave);
+            // 
+            // tbLower
+            // 
+            this.tbLower.Location = new System.Drawing.Point(147, 228);
+            this.tbLower.Name = "tbLower";
+            this.tbLower.Size = new System.Drawing.Size(100, 22);
+            this.tbLower.TabIndex = 15;
+            this.tbLower.Leave += new System.EventHandler(this.tbLower_Leave);
+            // 
+            // katasandi
+            // 
+            this.katasandi.AutoSize = true;
+            this.katasandi.Location = new System.Drawing.Point(12, 89);
+            this.katasandi.Name = "katasandi";
+            this.katasandi.Size = new System.Drawing.Size(79, 17);
+            this.katasandi.TabIndex = 16;
+            this.katasandi.Text = "Katasandi :";
+            // 
+            // epbenar
+            // 
+            this.epbenar.ContainerControl = this;
+            this.epbenar.Icon = ((System.Drawing.Icon)(resources.GetObject("epbenar.Icon")));
+            // 
+            // epwarn
+            // 
+            this.epwarn.ContainerControl = this;
+            this.epwarn.Icon = ((System.Drawing.Icon)(resources.GetObject("epwarn.Icon")));
+            // 
+            // Tampilkan
+            // 
+            this.Tampilkan.Location = new System.Drawing.Point(80, 262);
+            this.Tampilkan.Name = "Tampilkan";
+            this.Tampilkan.Size = new System.Drawing.Size(75, 23);
+            this.Tampilkan.TabIndex = 17;
+            this.Tampilkan.Text = "Tampikan";
+            this.Tampilkan.UseVisualStyleBackColor = true;
+            this.Tampilkan.Click += new System.EventHandler(this.Tampilkan_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(470, 297);
+            this.Controls.Add(this.Tampilkan);
+            this.Controls.Add(this.katasandi);
+            this.Controls.Add(this.tbLower);
+            this.Controls.Add(this.tbUpper);
+            this.Controls.Add(this.tbregex);
+            this.Controls.Add(this.tbCom2);
+            this.Controls.Add(this.tbCom1);
+            this.Controls.Add(this.tbLeng);
+            this.Controls.Add(this.tbNum);
+            this.Controls.Add(this.tbChar);
+            this.Controls.Add(this.Visi);
+            this.Controls.Add(this.Kota);
+            this.Controls.Add(this.Email);
+            this.Controls.Add(this.Membandingkan);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.Nik);
+            this.Controls.Add(this.Nama);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.epbenar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.epwarn)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Nama;
+        private System.Windows.Forms.Label Nik;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label Membandingkan;
+        private System.Windows.Forms.Label Email;
+        private System.Windows.Forms.Label Kota;
+        private System.Windows.Forms.Label Visi;
+        private System.Windows.Forms.TextBox tbChar;
+        private System.Windows.Forms.TextBox tbNum;
+        private System.Windows.Forms.TextBox tbLeng;
+        private System.Windows.Forms.TextBox tbCom1;
+        private System.Windows.Forms.TextBox tbCom2;
+        private System.Windows.Forms.TextBox tbregex;
+        private System.Windows.Forms.TextBox tbUpper;
+        private System.Windows.Forms.TextBox tbLower;
+        private System.Windows.Forms.Label katasandi;
+        private System.Windows.Forms.ErrorProvider epbenar;
+        private System.Windows.Forms.Button Tampilkan;
+        private System.Windows.Forms.ErrorProvider epwarn;
+    }
+}
+
---
+++ P4_percobaan4_1204013/P4_percobaan4_1204013/Form1.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace P4_percobaan4_1204013
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void tbChar_Leave(object sender, EventArgs e)
+        {
+            if (tbChar.Text != "")
+            {
+                epbenar.SetError(tbChar, "OK NAMA VALID");
+            }
+            else
+            {
+                epwarn.SetError(tbChar, "NAMA HARUS DIISI");
+            }
+        }
+
+        private void tbNum_Leave(object sender, EventArgs e)
+        {
+            int tbNumval;
+            if (int.TryParse(tbNum.Text, out tbNumval))
+            {
+                epbenar.SetError(tbNum, "OK");
+            }
+
+            else
+            {
+                epwarn.SetError(tbNum, "Silahkan Diisi");
+            }
+        }
+
+        private void tbLenght_Leave(object sender, EventArgs e)
+        {
+            if (tbLeng.Text != "")
+            {
+                int length = tbLeng.Text.Length;
+                if (length > 5)
+                {
+                    epbenar.SetError(tbLeng, "Password Valid");
+                }
+                else
+                {
+                    epwarn.SetError(tbLeng, "Password Erorr");
+                }
+
+            }
+            else
+            {
+                epwarn.SetError(tbLeng, "Silahkan Diisi");
+            }
+        }
+
+        private void tbCom1_Leave(object sender, EventArgs e)
+        {
+            if ((tbCom1.Text).All(Char.IsNumber))
+            {
+            }
+
+            else if (tbCom1.Text == "")
+            {
+                epwarn.SetError(tbCom1, "Textbox Huruf tidak boleh kosong");
+
+            }
+
+            else
+            {
+                epwarn.SetError(tbCom1, "inputan hanya boleh Angka!");
+            }
+        }
+
+        private void tbCom2_Leave(object sender, EventArgs e)
+        {
+            if ((tbCom2.Text).All(Char.IsNumber))
+            {
+                int Angka1;
+                int Angka2;
+                int.TryParse(tbCom1.Text, out Angka1);
+                int.TryParse(tbCom2.Text, out Angka2);
+                if (Angka1 > Angka2)
+                {
+                    epbenar.SetError(tbCom1, "Angka 1 lebih besar dari angka 2 ");
+                    epwarn.SetError(tbCom2, "Angka 2 lebih kecil dari angka 1 ");
+                }
+
+                else
+                {
+                    epbenar.SetError(tbCom2, "Angka 2 lebih besar dari angka 1 ");
+                    epwarn.SetError(tbCom1, "Angka 1 lebih kecil dari angka 2 ");
+                }
+            }
+
+            else if (tbCom2.Text == "")
+            {
+                epwarn.SetError(tbCom2, "Textbox Huruf tidak boleh kosong");
+
+            }
+
+            else
+            {
+                epwarn.SetError(tbCom2, "inputan hanya boleh Angka!");
+            }
+        }
+
+        private void tbregex_Leave(object sender, EventArgs e)
+        {
+            if (Regex.IsMatch(tbregex.Text, @"^^[^@\s]+@[^@\s]+(\.[^@\s]+)+$"))
+            {
+                epbenar.SetError(tbregex, "Betul!");
+            }
+            else
+            {
+                epwarn.SetError(tbregex, "Format salah !\nContoh: a@b.c");
+            }
+        }
+
+        private void tbUpper_Leave(object sender, EventArgs e)
+        {
+            if (tbUpper.Text.All(char.IsUpper))
+            {
+                epbenar.SetError(tbUpper, "OK");
+            }
+            else
+            {
+                epwarn.SetError(tbUpper, "Bukan UPPERCASE");
+            }
+        }
+
+        private void tbLower_Leave(object sender, EventArgs e)
+        {
+
+            if (tbLower.Text.All(char.IsLower))
+            {
+                epbenar.SetError(tbLower, "OK");
+            }
+            else
+            {
+                epwarn.SetError(tbLower, "Bukan LOWERCASE");
+            }
+        }
+
+        private void Tampilkan_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Nik : " + tbNum.Text + "\nNama : " + tbChar.Text + "\nEmail : " + tbregex.Text + "\nPerbandingan : " + tbCom1.Text + " " + tbCom2.Text + "\nKatasandi : " + tbLeng.Text + "\nKota dalam UPPERCASE : " + tbUpper.Text + "\nvisi dalam Lowercase : " + tbLower.Text);
+
+        }
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
